87252cdb4b351d73adcc41c0461673b29ddfb064
WULINJIANG1 <wulinjiang1@kingsoft.com>
2019-03-25 11:11:42 +0800

    [feat] 添加中文标签

--- easyBlog/users/forms.py
+++ easyBlog/users/forms.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 
 
 class UserRegisterForm(UserCreationForm):
-    email = forms.EmailField()
+    email = forms.EmailField(label='邮件地址')
 
     class Meta:
         model = User
